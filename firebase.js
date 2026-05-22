7e91b4171b3fca64264daf3a567a71194fa2047e
reviewwork <instantreviewwork@gmail.com>
2026-05-22 22:30:55 +0000

    firebase added

--- firebase.js
+++ firebase.js
@@ -1,23 +1,39 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+getDatabase,
+ref,
+set,
+push,
+get,
+child,
+update
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyDJP3t2KgKfQXgEnYZl8HBtIKP41wt8UOo",
+
   authDomain: "wallet-app-1421c.firebaseapp.com",
+
+  databaseURL: "https://wallet-app-1421c-default-rtdb.firebaseio.com",
+
   projectId: "wallet-app-1421c",
+
   storageBucket: "wallet-app-1421c.firebasestorage.app",
+
   messagingSenderId: "940151875275",
+
   appId: "1:940151875275:web:77962187250da21432dd4e",
+
   measurementId: "G-HCKQYETN4J"
+
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const db = getDatabase(app);
+
+export { db, ref, set, push, get, child, update };
